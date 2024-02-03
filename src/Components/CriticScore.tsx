b565d05aeadbcbaa4056c74788f42f127a899db9
shashankdangi <sss12212001@outlook.com>
2024-02-03 19:36:12 +0530

    Display Critic Score Badge

---
+++ src/Components/CriticScore.tsx
@@ -0,0 +1,20 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+const CriticScore = ({ score }: Props) => {
+  let color = score > 80 ? "green" : score > 60 ? "yellow" : "red";
+  return (
+    <Badge
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"4px"}
+      colorScheme={color}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
