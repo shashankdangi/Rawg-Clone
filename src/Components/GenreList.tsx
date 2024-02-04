ea147cdc629ae388c8359cc84c235bc1619d6bb9
shashankdangi <sss12212001@outlook.com>
2024-02-04 14:10:15 +0530

    Highlight the selected Genre {easy}

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -12,8 +12,9 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -32,6 +33,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
               variant={"link"}
               fontSize={"lg"}
               onClick={() => onSelectGenre(genre)}
+              fontWeight={genre.id === selectedGenre?.id ? "Bold" : "light"}
             >
               {genre.name}
             </Button>
