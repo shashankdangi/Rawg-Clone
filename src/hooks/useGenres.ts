5c7eb5bccabd185b61cce49fa8bc339f05c82564
shashankdangi <sss12212001@outlook.com>
2024-02-11 21:55:49 +0530

    Updated the error of useGenre hook

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -13,8 +13,7 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24 hrs
-    initialData: { count: genres.length, results: genres },
+    staleTime: 24 * 60 * 60 * 1000,
   });
 
 export default useGenres;
